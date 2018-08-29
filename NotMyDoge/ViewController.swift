43b3bd2986915e7845e7899d604bae5a86377778
Aaqib Hussain <iaaqibhussain@outlook.com>
2018-08-29 23:25:30 +0500

    Initial Commit

---
+++ NotMyDoge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NotMyDoge
+//
+//  Created by Aaqib Hussain on 29/8/18.
+//  Copyright © 2018 Aaqib Hussain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
